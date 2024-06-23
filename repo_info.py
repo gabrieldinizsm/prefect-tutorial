60dc44a547702f059041f7cee03066dd0b5a456b
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-06-23 01:36:39 -0300

    feat(get_repo_info): add retries

--- repo_info.py
+++ repo_info.py
@@ -2,7 +2,7 @@ import httpx
 from prefect import flow
 
 
-@flow(log_prints=True)
+@flow(retries=3, retry_delay_seconds=5, log_prints=True)
 def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     url = f"https://api.github.com/repos/{repo_name}"
     response = httpx.get(url)
@@ -12,6 +12,5 @@ def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     print(f"Stars 🌠 : {repo['stargazers_count']}")
     print(f"Forks 🍴 : {repo['forks_count']}")
 
-
 if __name__ == "__main__":
-    get_repo_info(repo_name="PrefectHQ/marvin")
+    get_repo_info()
