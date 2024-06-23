a7ed86b3c45d2614639c4a7ccd7629772326740d
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-06-23 01:34:09 -0300

    feat(get_repo_info): adding prefect logging

--- repo_info.py
+++ repo_info.py
@@ -2,15 +2,20 @@ import httpx
 from prefect import flow
 
 
+import httpx
+from prefect import flow, get_run_logger
+
+
 @flow
 def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     url = f"https://api.github.com/repos/{repo_name}"
     response = httpx.get(url)
     response.raise_for_status()
     repo = response.json()
-    print(f"{repo_name} repository statistics 🤓:")
-    print(f"Stars 🌠 : {repo['stargazers_count']}")
-    print(f"Forks 🍴 : {repo['forks_count']}")
+    logger = get_run_logger()
+    logger.info("%s repository statistics 🤓:", repo_name)
+    logger.info(f"Stars 🌠 : %d", repo["stargazers_count"])
+    logger.info(f"Forks 🍴 : %d", repo["forks_count"])
 
 
 if __name__ == "__main__":
